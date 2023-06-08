c1ae58e389b98e4f573ef8244908f8fa7114b858
JD <jdbriones22@gmail.com>
2023-06-08 18:45:34 +1200

    Added encore image

--- app/page.js
+++ app/page.js
@@ -1,21 +1,27 @@
+import Image from "next/image";
 export default function Home() {
   return (
-    <section className="w-full px-16">
-      <div className="grid grid-cols-3">
-        <div className="col-span-2">
-          <h1 className="text-7xl font-bold">
-            FIND YOUR PERFECT CONCERT WHEREVER YOU ARE
-          </h1>
-        </div>
-        <div className=" grid content-end">
-          <div>
-            <h1 className="text-2xl text-main-100 font-medium mb-4">
-              DISCOVER THE BEST LIVE MUSIC, TAILORED TO YOUR MUSIC TASTE
+    <>
+      <section className="w-full px-16">
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          <div className="col-span-2">
+            <h1 className="font-bold text-5xl md:text-8xl ">
+              FIND YOUR PERFECT CONCERT WHEREVER YOU ARE
             </h1>
-            <button className="accent-button">FIND A CONCERT</button>
+          </div>
+          <div className=" grid content-end">
+            <div>
+              <h1 className="text-3xl text-main-100 font-medium mb-4">
+                DISCOVER THE BEST LIVE MUSIC, TAILORED TO YOUR MUSIC TASTE
+              </h1>
+              <button className="accent-button">FIND A CONCERT</button>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="w-full px-16 mt-16 md:mt-32">
+        <Image src="/ENCORE.png" height={1080} width={1920}></Image>
+      </section>
+    </>
   );
 }
